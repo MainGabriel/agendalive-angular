57301aa787f38d56435599c381139abe0f654dae
Gabriel-developer-01 <gabriel.mendes.souza1@hotmail.com>
2020-07-25 10:54:07 -0300

    Criando um FormGroup

--- src/app/views/home/live-form-dialog/live-form-dialog.component.ts
+++ src/app/views/home/live-form-dialog/live-form-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-live-form-dialog',
@@ -7,12 +8,21 @@ import { MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./live-form-dialog.component.css']
 })
 export class LiveFormDialogComponent implements OnInit {
+  public liveForm: FormGroup
 
   constructor(
+    private fb: FormBuilder,
     public dialogRef: MatDialogRef<LiveFormDialogComponent>
   ) { }
 
   ngOnInit(): void {
+    this.liveForm = this.fb.group({
+      liveName: ['', [Validators.required]],
+      channelName: ['', [Validators.required]],
+      liveLink: ['', [Validators.required]],
+      liveDate: ['', [Validators.required]],
+      liveTime: ['', [Validators.required]],
+    });
   }
 
   cancel(): void {
